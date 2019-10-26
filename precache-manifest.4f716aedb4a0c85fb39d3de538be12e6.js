6101018a057f33f7110cc572fdcbf78c86549825
Kerwin Hy <kerwin.hy@gmail.com>
2019-10-26 00:36:10 -0700

    Updates

---
+++ precache-manifest.4f716aedb4a0c85fb39d3de538be12e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a4c3167fd90ff2401293fed6321bb0",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "786faef3521341456adf",
+    "url": "/memory-game/static/css/main.4bb03108.chunk.css"
+  },
+  {
+    "revision": "dfbc7d1f88bbb7beae3d",
+    "url": "/memory-game/static/js/2.4f20b97c.chunk.js"
+  },
+  {
+    "revision": "786faef3521341456adf",
+    "url": "/memory-game/static/js/main.795bc39d.chunk.js"
+  },
+  {
+    "revision": "e24248335f986e44ed3f",
+    "url": "/memory-game/static/js/runtime-main.baf69392.js"
+  }
+]);
